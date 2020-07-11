fbac194f4497117e03e720f85bf1ca973afd35a3
Corey Psoinos <corey@streetsmarts.io>
2020-07-11 19:49:25 -0400

    fix overflow

--- src/components/CongressPersonCard/CongressPersonCard.tsx
+++ src/components/CongressPersonCard/CongressPersonCard.tsx
@@ -22,23 +22,32 @@ export interface CongressPersonCardProps {
 const CongressPersonCard = (props: CongressPersonCardProps) => {
   const { avatar, lastName, firstName, party, usState, upForReElection, instagram, twitter, facebook, email, phone } = props
 
-  const imageClasses = classNames('h-auto flex-none rounded-t-none rounded-l object-cover overflow-hidden border-l-8', {
-    'border-background-blue': party === 'D',
-    'border-red-flag': party === 'R',
-    'border-gray-500': party === '***'
-  })
+  const imageClasses = classNames(
+    'h-auto',
+    'flex-none',
+    'rounded-t-none',
+    'rounded-l',
+    'object-cover',
+    'overflow-hidden',
+    'border-l-8',
+    {
+      'border-background-blue': party === 'D',
+      'border-red-flag': party === 'R',
+      'border-gray-500': party === '***'
+    }
+  )
 
   return (
     <div className="flex w-full">
       <img className={imageClasses} src={avatar} alt={`Senator ${lastName} ${firstName}`} width={200} />
 
-      <div className="border border-gray-400 bg-white rounded-r p-4 leading-normal w-full">
+      <div className="border border-gray-400 bg-white rounded-r p-4 leading-normal w-full overflow-auto">
         <div className="mb-4">
           <p className="text-sm text-gray-600 flex items-center font-lulo">
             {usState} - {party}
           </p>
           <div className="text-gray-900 font-bold text-xl mb-2 font-luloBold">{firstName} {lastName}</div>
-          <p className="text-gray-700 text-base text-xs italic">Up for re-election in {upForReElection}</p>
+          <p className="text-gray-700 text-xs italic">Up for re-election in {upForReElection}</p>
         </div>
 
         <ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -28,41 +28,43 @@ module.exports = {
         blue: '#00b3ff'
       }
     },
-    fontFamily: {
-      lulo: [
-        '"Lulo Clean W01 One"',
-        'sans-serif'
-      ],
-      luloBold: [
-        '"Lulo Clean W01 One Bold"',
-        'sans-serif'
-      ],
-      sans: [
-        'system-ui',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        '"Noto Sans"',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-        '"Noto Color Emoji"',
-      ],
-    },
-    fontSize: {
-      xxs: '0.5rem',
-    },
-    minWidth: {
-      '40': '10rem',
-      '1/4': '25%',
-      '1/3': `${(1/3) * 100}%`,
-    },
-    opacity: {
-      '10': '0.1',
+    extend: {
+      fontFamily: {
+        lulo: [
+          '"Lulo Clean W01 One"',
+          'sans-serif'
+        ],
+        luloBold: [
+          '"Lulo Clean W01 One Bold"',
+          'sans-serif'
+        ],
+        sans: [
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
+      fontSize: {
+        xxs: '0.5rem',
+      },
+      minWidth: {
+        '40': '10rem',
+        '1/4': '25%',
+        '1/3': `${(1/3) * 100}%`,
+      },
+      opacity: {
+        '10': '0.1',
+      }
     }
   },
   corePlugins: {},
