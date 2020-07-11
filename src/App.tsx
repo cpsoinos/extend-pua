a16a0da1d2858cbe39456735caf21653b84e43bb
Corey Psoinos <corey@streetsmarts.io>
2020-07-10 20:10:41 -0400

    add ability to sort

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import SenatorSocialHandles from './components/SenatorSocialHandles/SenatorSocia
 
 const App = () => {
   const styles = {
-    backgroundImage: "url(https://static.wixstatic.com/media/11062b_49bd140ff6034d019057a7dddc081e2c~mv2.jpg/v1/fill/w_1920,h_1280,al_t,q_85,usm_0.66_1.00_0.01/11062b_49bd140ff6034d019057a7dddc081e2c~mv2.webp)"
+    backgroundImage: "url(https://static.wixstatic.com/media/11062b_49bd140ff6034d019057a7dddc081e2c~mv2.jpg/v1/fill/w_1920,h_1280,al_t,q_85,usm_0.66_1.00_0.01/11062b_49bd140ff6034d019057a7dddc081e2c~mv2.jpg)"
   }
 
   return (
@@ -13,15 +13,11 @@ const App = () => {
           <div className="absolute bg-fixed bg-cover bg-top w-full h-full opacity-10 bg-no-repeat" style={styles}></div>
         </div>
       </div>
-      <div className="relative">
+      <div className="relative container mx-auto">
         <SenatorSocialHandles />
       </div>
-      {/* <header className="App-header"></header> */}
     </div>
   )
 }
 
 export default App;
-
-
-// position: absolute; top: 0px; width: 100%; opacity: 0.11; background-size: cover; background-position: center top; background-repeat: no-repeat; height: 100%; background-image: url("https://static.wixstatic.com/media/11062b_49bd140ff6034d019057a7dddc081e2c~mv2.jpg/v1/fill/w_1920,h_1280,al_t,q_85,usm_0.66_1.00_0.01/11062b_49bd140ff6034d019057a7dddc081e2c~mv2.webp");
--- src/components/SenatorSocialHandles/SenatorSocialHandles.tsx
+++ src/components/SenatorSocialHandles/SenatorSocialHandles.tsx
@@ -1,21 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import SenatorSocialInfoRow from '../SenatorSocialInfoRow/SenatorSocialInfoRow'
+import Button from '../Button/Button'
 import { useSenatorSocialHandles } from '../../hooks/useSenatorSocialHandles'
 import { SenatorSocialHandleRecord } from '../../types/SenatorSocialHandleResponse'
 import { useBuildSocialInfo } from '../../hooks/useBuildSocialInfo'
+import sortBy from 'lodash/sortBy'
 
 const SenatorSocialHandles = () => {
   const [senators, setSenators] = useState<SenatorSocialHandleRecord[]>([])
   const { getSenatorSocialHandles } = useSenatorSocialHandles()
   const { buildInstagram, buildFacebook, buildTwitter, buildEmailAndPhone } = useBuildSocialInfo()
+  const [orderBy, setOrderBy] = useState('st')
 
   useEffect(() => {
-    getSenatorSocialHandles().then(setSenators)
+    getSenatorSocialHandles().then((data) => {
+      setSenators(sortBy(data, orderBy))
+    })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+  useEffect(() => {
+    setSenators(sortBy(senators, orderBy))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [orderBy])
+
   return (
     <>
+      <div className="flex justify-around py-4">
+        <Button className="text-xxs" onClick={() => setOrderBy('st')}>Sort by state</Button>
+        <Button className="text-xxs" onClick={() => setOrderBy('party')}>Sort by party</Button>
+        <Button className="text-xxs" onClick={() => setOrderBy('reElection')}>Sort by re-election</Button>
+        <Button className="text-xxs" onClick={() => setOrderBy('last')}>Sort by name</Button>
+      </div>
       {senators.map((senator, i) => {
         return (
           <SenatorSocialInfoRow
