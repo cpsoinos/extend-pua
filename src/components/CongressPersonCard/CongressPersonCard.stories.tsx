b268eeb0855efad7f3afb41eff9c5dbb6d020ea4
Corey Psoinos <corey@streetsmarts.io>
2020-07-12 12:11:40 -0400

    fix missing props in stories

--- src/components/CongressPersonCard/CongressPersonCard.stories.tsx
+++ src/components/CongressPersonCard/CongressPersonCard.stories.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import CongressPersonCard from './CongressPersonCard'
+import { faInstagram, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faMailBulk, faPhone } from '@fortawesome/pro-regular-svg-icons'
 import withBaseStyles from '../../../.storybook/decorators/withBaseStyles'
 
 export default {
@@ -9,27 +11,33 @@ export default {
 }
 
 const instagram = {
-  platform: 'instagram',
   handle: '@KamalaHarris',
-  url: 'instagram.com/kamalaharris'
+  url: 'instagram.com/kamalaharris',
+  icon: faInstagram
 }
 
 const facebook = {
-  platform: 'facebook',
   handle: 'KamalaHarris',
-  url: 'facebook.com/kamalaharris'
+  url: 'facebook.com/kamalaharris',
+  icon: faFacebook
 }
 
 const twitter = {
-  platform: 'twitter',
   handle: '@KamalaHarris',
-  url: 'twitter.com/kamalaharris'
+  url: 'twitter.com/kamalaharris',
+  icon: faTwitter
 }
 
 const phone = {
-  platform: 'phone',
   handle: '(555) 867-5309',
-  url: 'tel:(555) 867-5309'
+  url: 'tel:(555) 867-5309',
+  icon: faPhone
+}
+
+const mail = {
+  handle: '(555) 867-5309',
+  url: 'tel:(555) 867-5309',
+  icon: faMailBulk
 }
 
 const props = {
@@ -43,19 +51,12 @@ const props = {
   facebook,
   twitter,
   phone,
+  mail
 }
 
 export const Default = () => {
   return (
     <div className="container mx-auto">
-      <CongressPersonCard {...props} />
-    </div>
-  )
-}
-
-export const WithBaseFont = () => {
-  return (
-    <div className="container mx-auto font-sans">
       <CongressPersonCard {...props} />
     </div>
   )
