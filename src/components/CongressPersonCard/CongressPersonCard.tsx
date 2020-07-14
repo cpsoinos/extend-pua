7d26ca51f99ada455f55c3ea5d8e67546135edfc
Corey Psoinos <corey@streetsmarts.io>
2020-07-13 22:13:19 -0400

    decent mobile view, broken larger

--- src/components/CongressPersonCard/CongressPersonCard.tsx
+++ src/components/CongressPersonCard/CongressPersonCard.tsx
@@ -22,9 +22,10 @@ const CongressPersonCard = (props: CongressPersonCardProps) => {
   const socialHandles = [facebook, twitter, instagram, phone, mail]
 
   const imageClasses = classNames(
-    'h-auto',
+    'sm:h-auto',
     'flex-none',
-    'rounded-t',
+    // 'rounded-full',
+    'sm:rounded-t',
     'rounded-r-none',
     'sm:rounded-t-none',
     'sm:rounded-l',
@@ -33,8 +34,10 @@ const CongressPersonCard = (props: CongressPersonCardProps) => {
     'border-b-8',
     'sm:border-b-0',
     'sm:border-l-8',
-    'w-full',
-    // 'sm:max-w-1/2',
+    'h-20',
+    'w-20',
+    // 'w-3/4',
+    // 'w-full',
     {
       'border-background-blue': party === 'D',
       'border-red-flag': party === 'R',
@@ -43,20 +46,20 @@ const CongressPersonCard = (props: CongressPersonCardProps) => {
   )
 
   return (
-    <div className="sm:flex w-full md:w-1/2 mb-4 px-0 sm:px-2 sm:rounded-md">
+    <div className="sm:flex w-full md:w-1/2 bg-white mb-4 px-0 sm:px-2 sm:rounded-md rounded-t">
       <div className="flex">
-        <img className={imageClasses} src={avatar} alt={`Senator ${lastName} ${firstName}`} />
-        {/* <div className="sm:hidden w-full bg-white leading-tight p-2">
+        <img className={imageClasses} src={avatar} alt={`Senator ${lastName} ${firstName}`} width={150} />
+        <div className="sm:hidden p-4">
           <p className="text-sm text-gray-600 font-lulo">{usState} - {party}</p>
           <small className="text-gray-700 text-xs italic">Up for re-election in {upForReElection}</small>
-        </div> */}
+        </div>
       </div>
 
       <div className="bg-white sm:rounded-r rounded-b p-2 leading-normal w-full overflow-auto">
         <div className="mb-4 leading-tight">
-          <p className="text-sm text-gray-600 font-lulo">{usState} - {party}</p>
+          <p className="hidden sm:inline-block text-sm text-gray-600 font-lulo">{usState} - {party}</p>
           <h3 className="text-gray-900 font-bold text-xl font-luloBold">{firstName} {lastName}</h3>
-          <small className="text-gray-700 text-xs italic">Up for re-election in {upForReElection}</small>
+          <small className="hidden sm:inline-block text-gray-700 text-xs italic">Up for re-election in {upForReElection}</small>
         </div>
 
         <ul>
