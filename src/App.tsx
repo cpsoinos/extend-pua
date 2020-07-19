2f5f25fc838d6e993299880f24f6977e2a224f33
Corey Psoinos <corey@streetsmarts.io>
2020-07-19 13:29:51 -0400

    catchall route redirects to extendpua.org; move bg img style to css

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,14 @@ import {
   Route,
   NavLink
 } from "react-router-dom"
+import RedirectToHome from 'pages/RedirectToHome/RedirectToHome'
 
 const App = () => {
-  const styles = {
-    backgroundImage: "url(https://static.wixstatic.com/media/11062b_49bd140ff6034d019057a7dddc081e2c~mv2.jpg/v1/fill/w_1920,h_1280,al_t,q_85,usm_0.66_1.00_0.01/11062b_49bd140ff6034d019057a7dddc081e2c~mv2.jpg)"
-  }
-
   return (
     <div className="relative">
       <div className="absolute">
         <div className="fixed bg-background-black h-full w-full">
-          <div className="absolute bg-fixed bg-cover bg-top w-full h-full opacity-10 bg-no-repeat" style={styles}></div>
+          <div className="bg-image-masks absolute bg-fixed bg-cover bg-top w-full h-full opacity-10 bg-no-repeat"></div>
         </div>
       </div>
       <div className="relative container mx-auto py-4 px-2">
@@ -32,10 +29,10 @@ const App = () => {
                   <NavLink to="/">Home</NavLink>
                 </li>
                 <li>
-                  <NavLink to="/handles">Senator Social Handles</NavLink>
+                  <NavLink to="/senators">Senator Social Handles</NavLink>
                 </li>
                 <li>
-                  <NavLink to="/awra-states">AWRA States</NavLink>
+                  <NavLink to="/states">AWRA States</NavLink>
                 </li>
               </ul>
             </nav>
@@ -43,11 +40,14 @@ const App = () => {
             {/* A <Switch> looks through its children <Route>s and
                 renders the first one that matches the current URL. */}
             <Switch>
-              <Route path="/handles">
+              <Route path="/senators">
                 <SenatorSocialHandles />
               </Route>
-              <Route path="/awra-states">
+              <Route path="/states">
                 <StatesList />
+              </Route>
+              <Route path="*">
+                <RedirectToHome />
               </Route>
             </Switch>
           </div>
