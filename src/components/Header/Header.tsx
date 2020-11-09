7b58e27b7034503fbbba9452c7de22c0400386a3
Corey Psoinos <corey@streetsmarts.io>
2020-11-09 08:36:03 -0500

    remove old lulo fonts; add nav to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,6 +3,34 @@ import Button from 'components/Button/Button'
 
 const LOGO_BLUE_SRC = 'https://res.cloudinary.com/extend-pua/image/upload/c_scale,w_1400,dpr_auto,q_auto/v1604794008/brand/Handles_XPUAHeader_Blue-PNG_nrwi0n.png'
 
+const links = [
+  {
+    text: 'HOME',
+    name: 'nav_HOME',
+    url: 'https://www.extendpua.org'
+  },
+  {
+    text: 'CALL SCRIPTS',
+    name: 'nav_CALL SCRIPTS',
+    url: 'https://www.extendpua.org/call'
+  },
+  {
+    text: 'TWEETS',
+    name: 'nav_TWEETS',
+    url: 'https://www.extendpua.org/your-senator'
+  },
+  {
+    text: 'DONATE',
+    name: 'nav_DONATE',
+    url: 'https://www.extendpua.org/donate'
+  },
+  {
+    text: 'LEARN MORE',
+    name: 'nav_LEARN MORE',
+    url: 'https://www.extendpua.org/the-mission'
+  },
+]
+
 const Header = () => {
   return (
     <div>
@@ -17,6 +45,11 @@ const Header = () => {
         <img src={LOGO_BLUE_SRC} alt="ExtendPUA logo" />
       </Button>
       <p className="font-luloBold text-center text-white text-xxs md:text-base md:tracking-widest">ADVOCATING FOR COMPREHENSIVE PANDEMIC RELIEF FOR ALL PEOPLE</p>
+      <nav className="flex justify-evenly font-luloBold text-xs text-white bg-brand-red border-white border-solid border-2 mx-4 mt-4">
+        {links.map((link) => {
+          return <Button key={link.text} href={link.url} name={link.name}>{link.text}</Button>
+        })}
+      </nav>
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'assets/fonts/FontsFree-Net-Lulo-Clean-W01-One-Bold.ttf'
-import 'assets/fonts/FontsFree-Net-Lulo-Clean-W01-One.ttf'
 import 'assets/styles/main.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
